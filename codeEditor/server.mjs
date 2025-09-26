7b0b5c1c7b0c20c1cf072b5e8ee17b3994d1f685
BimalSinha59 <bimalsinha6264@gmail.com>
2025-09-27 01:16:56 +0530

    modified:   codeEditor/server.mjs

--- codeEditor/server.mjs
+++ codeEditor/server.mjs
@@ -4,12 +4,17 @@ import { Server } from 'socket.io';
 import ACTIONS from './Actions.js';
 import dotenv from 'dotenv';
 import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
 
 app.use(express.static('dist'));
 
